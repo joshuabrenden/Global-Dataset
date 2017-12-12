c76f081fef55cf7d87198a84d5f7fc2134ae0501
jbrenden <joshuabrenden@gmail.com>
2017-12-12 13:28:35 -0700

    Initial commit

---
+++ src/main/java/com/josh/GlobalDatasetApplication.java
@@ -0,0 +1,12 @@
+package com.josh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalDatasetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalDatasetApplication.class, args);
+	}
+}
